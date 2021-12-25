497f68da81f4e0974eabadfdd72be7353446dfdd
Abdellatif Landolsi <abdellatif@landolsi.de>
2021-12-25 19:10:29 +0100

    first commit

---
+++ src/Js/main.js
@@ -0,0 +1,6 @@
+import '../Scss/style.module.scss'
+import '/node_modules/bootstrap/scss/bootstrap.scss'
+import { Tooltip, Toast, Popover } from 'bootstrap'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
